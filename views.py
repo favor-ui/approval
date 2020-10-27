bfdcfe147d4534079b2899f9f2ae8a3f85e07f3f
favor-ui <ferplore@gmail.com>
2020-10-27 23:39:30 +0100

    initial commit

--- views.py
+++ views.py
@@ -19,22 +19,72 @@ def index():
 
 
 
-@app.route('/enrollees', methods=['GET'])
-def get_enrollees():
+@app.route('/enrollson', methods=['GET'])
+def get_enrollson():
     # try:
-    mongo_data = mongo.db.enrollee
+    mongo_data = mongo.db.GS
     output = []
     for q in mongo_data.find({}):
-        output.append({"name":q["name"]})
+        output.append({"name": q["name"],
+                        "gender" : q["gender"],
+                        "lga" : q["lga"],
+                        "phone_number" : q["phone_number"],
+                        "genotype" : q["genotype"],
+                        "blood_group" : q["blood_group"],
+                        "ward" : q["ward"],
+                        "id" : q["id"],
+                        "dob" : q["dob"],
+                        "occupation" : q["occupation"],
+                        "next_of_kin" : q["next_of_kin"],
+                        "nok_contact" : q["nok_contact"]
+                    })
     
     return jsonify({"result": output})
     # except Exception:
     #     return jsonify({"Message":"Something went wrong Please check"})
 
-@app.route('/enrollee', methods=['GET'])
-def get_one_enrollee():
+
+
+
+@app.route('/enrollsof', methods=['GET'])
+def get_enrollsof():
     # try:
-    mongo_data = mongo.db.enrollee
+    mongo_data = mongo.db.enrollments
+    output = []
+    for q in mongo_data.find({}):
+        output.append({
+                        # "_id" : q["_id"],
+                        # "Submission ID": q["Submission ID"],
+                        "Name" : q["Name"],
+                        "Gender": q["Gender"],
+                        "Marital Status" : q["Marital Status"],
+                        "Date of Birth" : q["Date of Birth"],
+                        "Phone Number" : q["Phone Number"],
+                        "Email" : q["Email"],
+                        "Nationality" : q["Nationality"],
+                        "Occupation" : q["Occupation"],
+                        "Blood Group" : q["Blood Group"],
+                        "Genotype" : q["Genotype"],
+                        "LGA" : q["LGA"],
+                        "Ward" : q["Ward"],
+                        "Next of Kin" : q["Next of Kin"],
+                        "Next of kin Contact" : q["Next of kin Contact"],
+                        "ID Types" : q["ID Types"],
+                        # "ID Card":q["ID Card"],
+                        # "Passport" : q["Passport"],
+                        "Relationship" : q["Relationship"],
+                        # "Timestamp" : q["Timestamp"],
+                        "Status" : q["Status"]
+                    })
+    
+    return jsonify({"result": output})
+    # except Exception:
+    #     return jsonify({"Message":"Something went wrong Please check"})
+
+@app.route('/enrollon', methods=['GET'])
+def get_one_enrollon():
+    # try:
+    mongo_data = mongo.db.GS
     request_data = request.get_json()
     name1 = request_data['name']
     name2 = "_".join(name1.split())
@@ -54,69 +104,25 @@ def get_one_enrollee():
     # except Exception:
     #     abort(500)
 
+@app.route('/enrollof', methods=['GET'])
+def get_one_enrollof():
+    # try:
+    mongo_data = mongo.db.enrollments
+    request_data = request.get_json()
+    name1 = request_data['name']
+    name2 = "_".join(name1.split())
+    name = name2.lower()
+    
+    if not name:
+        return jsonify({"Error":"Field can not be blank", "status":0})
+    
+    q = mongo_data.find_one({"Name":name})
 
-
-# @app.errorhandler(400)
-# def bad_request__error(exception):
-#     return jsonify(
-#         {
-#             "Message": "Sorry you entered wrong values kindly check and resend!"
-#         },
-#         {
-#             "status":400
-#         }
-#     )
-
-
-# @app.errorhandler(401)
-# def internal_error(error):
-#     return jsonify(
-#         {
-#             "Message": "Acess denied ! please register and login to generate API KEY"
-#         },
-#         {
-#             "status": 401
-#         }
-#     )
-
-
-
-# @app.errorhandler(404)
-# def not_found_error(error):
-#     return jsonify(
-#         {
-#             "Message":"Sorry the page your are looking for is not here kindly go back"
-#         },
-#         {
-#             "status": 404
-#         }
-#     )
-
-
-
-
-
-# @app.errorhandler(405)
-# def methodNotallowes(error):
-#     return jsonify(
-#         {
-#             "Message": "Sorry the requested method is not allowed kindly check and resend !"
-#         },
-#         {
-#             "status": 405
-#         }
-#     )
-
-# @app.errorhandler(500)
-# def method_not_allowed(error):
-#     return jsonify(
-#         {
-#             "Message": "Bad request please check your input and resend !"
-#         },
-#         {
-#             "status": 500
-#         }
-#     )
-
-# if __name__=='__main__':
-#     app.run(debug=True)
+    if q:
+        output = {"Enrollee":q["name"]}
+    else:
+        output = "No results"
+    
+    return jsonify({"result": output})
+    # except Exception:
+    #     abort(500)
